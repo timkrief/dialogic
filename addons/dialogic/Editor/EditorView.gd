61257ab4970cf72176875a0ee3367abf01cc34c1
Emilio Coppola <coppolaemilio@gmail.com>
2021-04-04 12:29:06 +0200

    Making the function to get the current scale

--- addons/dialogic/Editor/EditorView.gd
+++ addons/dialogic/Editor/EditorView.gd
@@ -16,6 +16,8 @@ onready var settings_editor = $MainPanel/SettingsEditor
 
 
 func _ready():
+	editor_scale = get_editor_scale()
+
 	# Adding file dialog to get used by pieces
 	editor_file_dialog = EditorFileDialog.new()
 	add_child(editor_file_dialog)
@@ -167,3 +169,26 @@ func _on_Logo_gui_input(event) -> void:
 func dprint(what) -> void:
 	if debug_mode:
 		print(what)
+
+
+func get_editor_scale():
+	# This will be replaced once the variable gets exposed to gdscript. 
+	# pycbouh [Жыве Беларусь!] told me on discord that a PR is comming.
+	var _scale = get_constant("inspector_margin", "Editor")
+	var result = 1
+	
+	match _scale:
+		6:
+			result = 0.75
+		8:
+			result = 1;
+		10: 
+			result = 1.25
+		12: 
+			result = 1.5
+		14:
+			result = 1.75
+		16:
+			result = 2
+
+	return result
